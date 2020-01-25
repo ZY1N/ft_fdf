f141b4fabd3f1a0d861c1b2e7f1399622fb79319
ZY1N <zhang.yine@husky.neu.edu>
2020-01-25 13:33:11 -0800

    abc

--- draw_line.c
+++ draw_line.c
@@ -17,51 +17,32 @@ void	drawl(t_vector f, t_vector s, t_map map)
 	t_vector	delta;
 	t_vector	sign;
 	t_vector	cur;
-	int			error[2];
+	int			error1;
+	int			error2;
 
 	delta.x = abs(s.x - f.x);
-	delta.y = abs(s.y - f.y);
 	sign.x = f.x < s.x ? 1 : -1;
+	delta.y = -abs(s.y - f.y);
 	sign.y = f.y < s.y ? 1 : -1;
-	error[0] = delta.x - delta.y;
+	error1 = delta.x + delta.y;
 	cur = f;
 	while (cur.x != s.x || cur.y != s.y)
 	{
-		if(map.gradon == 1)
-			mlx_pixel_put(map.mlx_ptr, map.win_ptr, cur.x, cur.y, map.color + (map.coloradj * cur.x));
+		error2 = 2 * error1;
+		if (map.gradon == 1)
+			mlx_pixel_put(map.mlx_ptr, map.win_ptr, cur.x, cur.y,
+				map.color + (map.coloradj * cur.x));
 		else
 			mlx_pixel_put(map.mlx_ptr, map.win_ptr, cur.x, cur.y, map.color);
-		if ((error[1] = error[0] * 2) > -delta.y)
+		if (error2 >= delta.y)
 		{
-			error[0] -= delta.y;
+			error1 += delta.y;
 			cur.x += sign.x;
 		}
-		if (error[1] < delta.x)
+		if (error2 <= delta.x)
 		{
-			error[0] += delta.x;
+			error1 += delta.x;
 			cur.y += sign.y;
 		}
 	}
 }
-
-/*
-void blinealgo(t_vector f, t_vector s, void *mlx_ptr, void * win_ptr)
-{
-    t_vector delta;
-    t_vector sign;
-    t_vector cur;
-    int error[2];
-
-    delta.x = abs(s.x - f.x)
-    delta.y = abs(s.y - f.y)
-    sign.x = f.x < s.x ? 1 : -1;
-    sign.y = f.y < s.y ? 1 : -1;
-    error[0] = delta.x - delta.y;
-    cur = f;
-
-    while(cur.x != s.x || cur.y != s.y)
-    {
-        mlx_pixel_put(mlx_ptr, win_ptr, cur.x, cur.y, 123123312);
-        if((error[1] = error[0] * 2) > -delta.y)
-    }
-} */
